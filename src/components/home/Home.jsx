3e9cca34e746d62ed32b39f6b7b3a7480fa666e1
Ashish_Jain <121597042+asj0000@users.noreply.github.com>
2023-09-07 10:22:20 +0530

    added data in redux store

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,11 @@
- //components
+import { useEffect } from 'react';
+
+//components
  import Navbar from './Navbar';
  import Banners from './Banners';
  import{ Box , styled } from '@mui/material';
-
+ import { getProducts } from '../../redux/action/productActions';
+ import { useDispatch , useSelector } from 'react-redux';
 
 
  const Component = styled(Box)  `
@@ -11,6 +14,16 @@
      
  `
 const Home = () => {
+
+   const { products } = useSelector(state => state.getProducts)
+
+   const dispatch = useDispatch();
+
+   useEffect(()=>{
+   dispatch( getProducts())
+   
+   } ,[dispatch])
+
   return (
     //created a parent component because JSX does not allow more than one parent component
     <> 
---
+++ src/redux/constants/productConstants.js
@@ -0,0 +1,5 @@
+
+
+
+export const GET_PRODUCTS_SUCCESS = 'getProductsuccess';
+export const GET_PRODUCTS_FAIL = 'getProductfail';
